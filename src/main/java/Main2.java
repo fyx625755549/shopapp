daf9affb5e456f1171d752d597f36526f4c91e10
冯有旭 <625755549@qq.com>
2021-04-03 22:21:06 +0800

    冯有旭

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,7 +1,6 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.println(111);
-        System.out.println("创建分支");
-        System.out.println("添加分支");
+        System.out.println("添加主干");
     }
 }
